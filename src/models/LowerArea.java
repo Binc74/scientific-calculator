c961d692ec3b6bd029e4790cfb542706f679d656
Chen Bin <chen.6133@osu.edu>
2018-11-30 20:52:02 -0500

    Add models

--- src/models/LowerArea.java
+++ src/models/LowerArea.java
@@ -1,5 +1,19 @@
 package models;
 
+/**
+ * Model for the lower display area.
+ * 
+ * @author Bin Chen
+ */
 public class LowerArea {
-
+	
+	private double num;
+	
+	public LowerArea() {
+		num = 0;
+	}
+	
+	public String toString() {
+		return Double.toString(num);
+	}
 }
